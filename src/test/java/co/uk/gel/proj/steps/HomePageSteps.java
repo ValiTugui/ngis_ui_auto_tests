b2ab49195d76cab89712b39aa21353cfe31e5bf3
cs97aad <cs97aad@yahoo.co.uk>
2019-10-15 07:59:04 +0100

    Updated step-defs-homepage
    Updated step-defs

--- src/test/java/co/uk/gel/proj/steps/HomePageSteps.java
+++ src/test/java/co/uk/gel/proj/steps/HomePageSteps.java
@@ -1,10 +1,13 @@
 package co.uk.gel.proj.steps;
 
 import co.uk.gel.config.SeleniumDriver;
+import co.uk.gel.lib.Click;
+import co.uk.gel.lib.Wait;
 import co.uk.gel.proj.pages.Pages;
 import cucumber.api.java.en.Then;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.By;
 
 
 public class HomePageSteps extends Pages {
