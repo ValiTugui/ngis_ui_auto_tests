69131a4284613fa62c159ebf83733c3c437d3d40
sspriya4789 <55276432+sspriya4789@users.noreply.github.com>
2019-11-08 12:08:55 +0000

    Update HomePage.java

--- src/main/java/co/uk/gel/proj/pages/HomePage.java
+++ src/main/java/co/uk/gel/proj/pages/HomePage.java
@@ -205,7 +205,7 @@ public class HomePage {
 
     public String closeCookiesButton = "//*[contains(@class,'cta__')]//descendant::button";
 
-    @FindBy(className = "styles_link__2terw")
+    @FindBy(xpath = "//*[contains(@class, 'styles_link')]")
     public List<WebElement> tabResults;
 
 
@@ -278,6 +278,6 @@ public class HomePage {
     }
 
     public boolean testResultsAreLoaded() {
-        return tabResults.size() == 10;
+        return tabResults.size()>0;
     }
 }
