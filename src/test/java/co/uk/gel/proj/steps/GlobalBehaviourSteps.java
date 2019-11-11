d60c9bcb25bedc4a91a50e6d1f8e4d2ae92f5bad
sspriya4789 <55276432+sspriya4789@users.noreply.github.com>
2019-11-11 13:58:26 +0000

    1658 Update

--- src/test/java/co/uk/gel/proj/steps/GlobalBehaviourSteps.java
+++ src/test/java/co/uk/gel/proj/steps/GlobalBehaviourSteps.java
@@ -1,6 +1,8 @@
 package co.uk.gel.proj.steps;
 
 import co.uk.gel.config.SeleniumDriver;
+import co.uk.gel.lib.Actions;
+import co.uk.gel.lib.Click;
 import co.uk.gel.proj.pages.Pages;
 import co.uk.gel.proj.util.Debugger;
 import io.cucumber.java.en.*;
@@ -18,4 +20,21 @@ public class GlobalBehaviourSteps extends Pages {
         Assert.assertTrue("The NGIS version number is NOT present next to the privacy policy link", globalBehaviourPage.isNGISVersionPresent());
     }
 
+    @Then("the user can see the {string} link at bottom of the page")
+    public void theUserCanSeeTheLinkAtBottomOfThePage(String expectedText) {
+        Assert.assertTrue(globalBehaviourPage.isPrivacyPolicyLinkPresent(expectedText));
+        Assert.assertTrue(globalBehaviourPage.footerLinks.get(1).getAttribute("href").contains("privacy-policy"));
+    }
+
+    @When("the user clicks the privacy policy link")
+    public void theUserClicksThePrivacyPolicyLink() {
+        globalBehaviourPage.clickPrivacyPolicy();
+    }
+
+    @Then("the {string} page should be opened in the next tab")
+    public void thePageShouldBeOpenedInTheNextTab(String pageTitle) {
+        Assert.assertTrue(driver.getCurrentUrl().contains("privacy-policy"));
+        Assert.assertTrue(globalBehaviourPage.checkPrivacyPolicyLinkPage(pageTitle));
+        Actions.switchTab(driver);
+    }
 }
