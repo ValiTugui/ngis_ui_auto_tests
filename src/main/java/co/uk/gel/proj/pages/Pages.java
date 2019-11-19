da63f1cf9d6f6da1e3a041114cf75cbad7e2e986
Krishan Shukla <krishan.shyam@gmail.com>
2019-11-19 15:30:17 +0000

    debugging error due to browser set to null

--- src/main/java/co/uk/gel/proj/pages/Pages.java
+++ src/main/java/co/uk/gel/proj/pages/Pages.java
@@ -126,6 +126,7 @@ public class Pages implements Navigable {
     @Override
     public void switchToURL(String currentURL) {
         Debugger.println("CURRENT URL: " + currentURL);
+            Wait.seconds(6);
         if (driver.getCurrentUrl().contains(patientSearchURL)) {
             Actions.cleanUpSession(driver);
         } else if (driver.getCurrentUrl().contains(testOrderLoginURL) || driver.getCurrentUrl().contains(testOrderURL)) {
