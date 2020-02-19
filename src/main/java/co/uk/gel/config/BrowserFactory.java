427fcf2e060a85c1d9f3859173f6d97a58006e07
Krishan Shukla <krishan.shyam@gmail.com>
2020-02-19 11:52:26 +0000

    Adding Linux support for chromedriver

--- src/main/java/co/uk/gel/config/BrowserFactory.java
+++ src/main/java/co/uk/gel/config/BrowserFactory.java
@@ -29,7 +29,7 @@ public class BrowserFactory {
     private final static int ZAP_PROXYPORT = 9191;
     private final static String ZAP_APIKEY = null;
     private final static String CHROME_DRIVER_PATH = "/Users/krishanshukla/Library/Application Support/ZAP/webdriver/macos/64/chromedriver";
-    private final static String CHROME_DRIVER_UBUNTU = "/usr/lib/chromium-browser/chromedriver";
+    private final static String CHROME_DRIVER_UBUNTU = "/usr/lib/chromedriver";
     private final static String CHROME_DRIVER_PATH_On_TEST_MACHINE = "C:\\Users\\Testing Team\\OWASP ZAP\\webdriver\\windows\\32\\chromedriver.exe";
     private final static String MEDIUM = "MEDIUM";
     private final static String HIGH = "HIGH";
