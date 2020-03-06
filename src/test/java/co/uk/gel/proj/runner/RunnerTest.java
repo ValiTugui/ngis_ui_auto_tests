5fcb9f6b5f570625119b4bd76e3514a4fe80e4b7
Santhosh P S <santhosh.s@stagsoftware.com>
2020-03-06 05:46:17 +0530

    Added more exception handling and checks to the method searchAndSelectRandomHPOPhenotype in ClinicalQuestionsPage

--- src/test/java/co/uk/gel/proj/runner/RunnerTest.java
+++ src/test/java/co/uk/gel/proj/runner/RunnerTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
          plugin = {"pretty", "html:target/cucumber","json:target/cucumber.json"},
          glue = {"co.uk.gel.proj.steps"},
          features = {"src/test/resources/features"},
-         tags = {"@E2EUI-1157"}
+         tags = {"@RD_DUO"}
         )
     public class RunnerTest {
         @BeforeClass
