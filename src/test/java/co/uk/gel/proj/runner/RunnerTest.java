5ce1d0da69ef3246f2d17d34b2668480a5449b8a
Krishan Shukla <krishan.shyam@gmail.com>
2020-03-20 16:23:48 +0000

    updated the runner

--- src/test/java/co/uk/gel/proj/runner/RunnerTest.java
+++ src/test/java/co/uk/gel/proj/runner/RunnerTest.java
@@ -1,21 +1,21 @@
 package co.uk.gel.proj.runner;
 
 import co.uk.gel.proj.util.Debugger;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
+        import io.cucumber.junit.Cucumber;
+        import io.cucumber.junit.CucumberOptions;
+        import org.junit.BeforeClass;
+        import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-         plugin = {"pretty", "html:target/cucumber","json:target/cucumber.json"},
-         glue = {"co.uk.gel.proj.steps"},
-         features = {"src/test/resources/features"},
-         tags = {"@securityscan_rd"}
-        )
-    public class RunnerTest {
-        @BeforeClass
-        public static void setup() {
-            Debugger.println("\n******* RUN STARTS " + new java.util.Date() + " *******************************");
-        }
-    }//end
+        plugin = {"pretty", "html:target/cucumber","json:target/cucumber.json"},
+        glue = {"co.uk.gel.proj.steps"},
+        features = {"src/test/resources/features"},
+        tags = {"@securitytest"}
+)
+public class RunnerTest {
+    @BeforeClass
+    public static void setup() {
+        Debugger.println("\n******* RUN STARTS " + new java.util.Date() + " *******************************");
+    }
+}//end
