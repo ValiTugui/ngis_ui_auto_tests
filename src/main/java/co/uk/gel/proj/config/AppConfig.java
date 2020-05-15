92479f405685f4b36f1ef9ec1de9b6d21c31af0c
Santhosh P S <santhosh.s@stagsoftware.com>
2020-05-15 16:46:32 +0530

    Snapshots for each step in BVT for Beta

--- src/main/java/co/uk/gel/proj/config/AppConfig.java
+++ src/main/java/co/uk/gel/proj/config/AppConfig.java
@@ -35,8 +35,7 @@ public class AppConfig {
         String current_environment = System.getProperty("TestEnvironment");
         System.out.println("TestEnvironment: " + current_environment);
         if(current_environment.equalsIgnoreCase("UAT") ||
-        current_environment.equalsIgnoreCase("Beta") ||
-                current_environment.equalsIgnoreCase("Demo")){
+        current_environment.equalsIgnoreCase("Beta")){
             snapshotRequired = true;
         }
         configFileName = String.format(configFileName, current_environment);
