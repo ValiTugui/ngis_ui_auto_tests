c93926857d5a048ca8130db4f666ec1b91a933e0
sspriya4789 <55276432+sspriya4789@users.noreply.github.com>
2020-08-17 15:13:14 +0100

    Update BrowserFactory.java

--- src/main/java/co/uk/gel/config/BrowserFactory.java
+++ src/main/java/co/uk/gel/config/BrowserFactory.java
@@ -242,7 +242,7 @@ public class BrowserFactory {
         chromeLocalOptions.addArguments("--disable-gpu");
         chromeLocalOptions.addArguments("--no-sandbox");
         chromeLocalOptions.addArguments("--headless");
-        chromeLocalOptions.AddArgument("--whitelisted-ips");
+        chromeLocalOptions.addArgument("--whitelisted-ips");
         if (null != userAgent) {
             chromeLocalOptions.addArguments("user-agent=" + userAgent);
         }
