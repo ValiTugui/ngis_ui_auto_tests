236753798b6baca01206c1d3db123e5e1955ea14
Kundan Kumar <kundan.k@stagsoftware.com>
2020-08-18 17:56:08 +0530

    NTS-3418: Jenkins related failure has been fixed

--- src/main/java/co/uk/gel/config/BrowserFactory.java
+++ src/main/java/co/uk/gel/config/BrowserFactory.java
@@ -239,10 +239,10 @@ public class BrowserFactory {
     private ChromeOptions getChromeLocalOptions(String userAgent,
                                                 boolean javascriptEnabled) {
         ChromeOptions chromeLocalOptions = new ChromeOptions();
-        chromeLocalOptions.addArguments("--disable-gpu");
-        chromeLocalOptions.addArguments("--no-sandbox");
-        chromeLocalOptions.setHeadless(true);
-        chromeLocalOptions.addArguments("--whitelisted-ips");
+//        chromeLocalOptions.addArguments("--disable-gpu");
+//        chromeLocalOptions.addArguments("--no-sandbox");
+//        chromeLocalOptions.setHeadless(true);
+//        chromeLocalOptions.addArguments("--whitelisted-ips");
         if (null != userAgent) {
             chromeLocalOptions.addArguments("user-agent=" + userAgent);
         }
@@ -398,10 +398,10 @@ public class BrowserFactory {
         } catch (Exception exp) {
             System.out.println("Exception in creating download directory..." + exp);
         }
-        profile.setPreference("browser.cache.disk.enable", false);
-        profile.setPreference("browser.cache.memory.enable", false);
-        profile.setPreference("browser.cache.offline.enable", false);
-        profile.setPreference("network.http.use-cache", false);
+//        profile.setPreference("browser.cache.disk.enable", false);
+//        profile.setPreference("browser.cache.memory.enable", false);
+//        profile.setPreference("browser.cache.offline.enable", false);
+//        profile.setPreference("network.http.use-cache", false);
         profile.setPreference("browser.download.folderList", 2);
         profile.setPreference("browser.download.dir", downloadFilepath);
         profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "text/csv,application/msword, application/json, application/ris, participant_id/csv, image/png, application/pdf, participant_id/html, participant_id/plain, application/zip, application/x-zip, application/x-zip-compressed, application/download, application/octet-stream");
