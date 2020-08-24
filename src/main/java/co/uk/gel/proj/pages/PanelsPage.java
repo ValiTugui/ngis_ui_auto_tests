a135ef9b44f6a6ab14293aa175ce4c6dcbfcb44c
Syamalaushas <syamala.u@stagsoftware.com>
2020-08-24 17:54:58 +0530

    Concurrency Scripts modified

--- src/main/java/co/uk/gel/proj/pages/PanelsPage.java
+++ src/main/java/co/uk/gel/proj/pages/PanelsPage.java
@@ -6,12 +6,17 @@ import co.uk.gel.lib.SeleniumLib;
 import co.uk.gel.lib.Wait;
 import co.uk.gel.proj.config.AppConfig;
 import co.uk.gel.proj.util.Debugger;
-import org.openqa.selenium.*;
+import co.uk.gel.proj.util.TestUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 public class PanelsPage {
 
