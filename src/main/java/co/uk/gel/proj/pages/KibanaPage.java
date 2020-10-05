becc2a54661757227d5a28994ef8a7b28ab5d5cc
Santhosh P S <santhosh.s@stagsoftware.com>
2020-10-05 20:02:18 +0530

    Kibana is done

--- src/main/java/co/uk/gel/proj/pages/KibanaPage.java
+++ src/main/java/co/uk/gel/proj/pages/KibanaPage.java
@@ -4,6 +4,7 @@ import co.uk.gel.lib.Actions;
 import co.uk.gel.lib.Click;
 import co.uk.gel.lib.SeleniumLib;
 import co.uk.gel.lib.Wait;
+import co.uk.gel.proj.config.AppConfig;
 import co.uk.gel.proj.util.Debugger;
 import io.cucumber.datatable.DataTable;
 import org.openqa.selenium.By;
@@ -12,6 +13,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class KibanaPage {
@@ -27,35 +29,78 @@ public class KibanaPage {
     @FindBy(xpath = "(//div[contains(@class,'euiFormControlLayout__childrenWrapper')])[1]")
     public WebElement searchBox;
 
+    @FindBy(xpath = "//input[@data-test-subj='queryInput']")
+    public WebElement searchInput;
+
     @FindBy(xpath = "//div[@id='QuickSelectPopover']//button")
     public WebElement dateIcon;
 
+    @FindBy(xpath = "//ul[contains(@class,'dscFieldList--popular')]/li//field-name")
+    public List<WebElement> popularFilterList;
+    @FindBy(xpath = "//ul[contains(@class,'dscFieldList--unpopular')]/li//field-name")
+    public List<WebElement> unPopularFilterList;
+
+    @FindBy(xpath = "//button[@class='kuiLocalMenuItem '][text()='Save']")
+    public WebElement saveButton;
+    @FindBy(xpath = "//input[@data-test-subj='savedObjectTitle']")
+    public WebElement reportNameInput;
+
+    @FindBy(xpath = "//button[@data-test-subj='confirmSaveSavedObjectButton']")
+    public WebElement confirmSaveButton;
+    @FindBy(xpath = "//button[@class='kuiLocalMenuItem '][text()='Share']")
+    public WebElement shareButton;
+    @FindBy(xpath = "//button[@data-test-subj='sharePanel-CSVReports']")
+    public WebElement csvReports;
+    @FindBy(xpath = "//button[@data-test-subj='generateReportButton']")
+    public WebElement generateCSV;
+
     String buttonAction = "//span[contains(@class,'euiButton__content')]//span[text()='dummyName']";
 
-    @FindBy(xpath = "//div[@class='euiPanel euiPanel--paddingMedium euiPopover__panel euiPopover__panel--left euiPopover__panel-isOpen']")
-    public WebElement datePanel;
+    public String loadKibanaPage(String baseURLKibana){
+        try{
+            String KibanaPage = AppConfig.getPropertyValueFromPropertyFile(baseURLKibana);
+            Wait.forPageToBeLoaded(driver);
+            driver.get(KibanaPage);
+            Wait.seconds(5);
+            return "Success";
+        }catch(Exception exp){
+            return "Exception in accessing Kibana Page:"+exp;
+        }
 
-    String datelink = "//div[contains(@class,'euiFlexGrid--halves')]//button[text()='dummyLink']";
+    }
 
-    public boolean searchReferralId(String refID) {
+    public String searchReferralId(String refID,String commonLink) {
         try {
-            Wait.seconds(20);
             if (!Wait.isElementDisplayed(driver, searchBox, 30)) {
-                Debugger.println("The KIBANA tool page search box is not displayed.");
                 SeleniumLib.takeAScreenShot("KibanaSearchError.jpg");
-                return false;
-            }
-            Wait.seconds(12);//wait for the search result to load
+                return "The KIBANA tool page search box is not displayed.";
+             }
             Debugger.println("The Ref ID to search:" + refID);
-//            seleniumLib.sendValue(searchBox, refID);
-            searchBox.click();
-            searchBox.sendKeys(refID);
-             Wait.seconds(2);//wait for the search result to load
-            return true;
+            seleniumLib.clickOnWebElement(searchBox);
+            seleniumLib.sleepInSeconds(2);
+            Debugger.println("Clicked:");
+            //searchBox.click();
+            seleniumLib.sendValue(searchInput,refID);
+            seleniumLib.sleepInSeconds(2);
+            if(commonLink == null || commonLink.isEmpty()){
+                return "Success";//No specific duration to be selected
+            }
+            seleniumLib.clickOnWebElement(dateIcon);
+            seleniumLib.sleepInSeconds(2);
+            //Commonly used link
+            if(commonLink.indexOf("-") == -1) {
+                By commonUsedLink = By.xpath("//button[@class='euiLink euiLink--primary'][text()='" + commonLink + "']");
+                seleniumLib.clickOnElement(commonUsedLink);
+                seleniumLib.sleepInSeconds(10);
+            }else{
+                String[] dates = commonLink.split("-");
+                //To be done
+            }
+
+            return "Success";
         } catch (Exception exp) {
-            Debugger.println("Exception from searchReferralId:" + exp);
-            SeleniumLib.takeAScreenShot("searchReferralId.jpg");
-            return false;
+            SeleniumLib.takeAScreenShot("KibanaSearchReferralId.jpg");
+            return "Exception from searchReferralId:" + exp;
         }
     }
 
@@ -77,30 +122,155 @@ public class KibanaPage {
         }
     }
 
-    public boolean selectTheDateLink(List<String> link) {
+    public String selectFilter(String filter) {
         try {
-            String selectedLink = link.get(0);
-            String linkName = datelink.replaceAll("dummyLink", selectedLink);
-            WebElement date = driver.findElement(By.xpath(linkName));
-
-            Wait.forPageToBeLoaded(driver);
-            if (!Wait.isElementDisplayed(driver, dateIcon, 20)) {
-                Debugger.println("Date Icon button is not displayed");
-                SeleniumLib.takeAScreenShot("selectTheDateLink.jpg");
-                return false;
+            boolean isPresent = false;
+            String actTitle = "";
+            WebElement buttoElement = null;
+            for (WebElement webElement : popularFilterList) {
+                actTitle = webElement.getAttribute("title");
+                Debugger.println("Popular: "+actTitle);
+                if(actTitle.contains(filter)){
+                    isPresent = true;
+                    webElement.click();
+                    buttoElement = webElement.findElement(By.xpath("./../button"));
+                    if(seleniumLib.isElementPresent(buttoElement)) {
+                        Debugger.println("Added....");
+                        seleniumLib.clickOnWebElement(buttoElement);
+                    }
+                    break;
+                }
+                seleniumLib.sleepInSeconds(2);
             }
-            Actions.clickElement(driver, dateIcon);
-            Wait.seconds(20);
-            Wait.forElementToBeDisplayed(driver, date);
-            Click.element(driver, date);
-            Wait.seconds(10);
-            return true;
+            if(isPresent){
+                return "Success";
+            }
+            for (WebElement webElement : unPopularFilterList) {
+                actTitle = webElement.getAttribute("title");
+                Debugger.println("UnPopular: "+actTitle);
+                if(actTitle.contains(filter)){
+                    isPresent = true;
+                    buttoElement = webElement.findElement(By.xpath("./../button"));
+                    seleniumLib.clickOnWebElement(buttoElement);
+                    break;
+                }
+                seleniumLib.sleepInSeconds(2);
+            }
+            if(!isPresent){
+                return "Filter "+filter+" could not find.";
+            }
+            return "Success";
         } catch (Exception exp) {
             Debugger.println("Exception from selectTheDateLink:" + exp);
             SeleniumLib.takeAScreenShot("selectTheDateLink.jpg");
-            return false;
+            return "Exception from selectTheDateLink:" + exp;
         }
+    }
+    public String verifySearchResults(String tableFields){
+        try{
+            By tableHead = By.xpath("//table[@data-test-subj='docTable']/thead/tr/th/span");
+            if(!seleniumLib.isElementPresent(tableHead)){
+                if(!seleniumLib.isElementPresent(tableHead)){
+                    return "Kibana Search Result not displayed.";
+                }
+            }
+            String colums[] = tableFields.split(",");
+            int expCols = colums.length;
+            List <WebElement> tableHeads = seleniumLib.getElements(tableHead);
+            int count = 0;
+            for (WebElement head : tableHeads) {
+                //Debugger.println("Head: "+head.getText());
+                if(tableFields.contains(head.getText())){
+                    count++;
+                }
+            }
+            if(expCols != count){
+                return "All the selected fields are not loaded in Kibana Report.";
+            }
+            Debugger.println("Count: "+expCols+","+count);
+            //Table Rows
+            List <WebElement> searchResults = seleniumLib.getElements(By.xpath("//table[@data-test-subj='docTable']/tbody/tr"));
+            if(searchResults.size() < 1){
+                return "No search results have been loaded for the Kibana Filters.:"+tableFields;
+            }
+            return "Success";
 
+        }catch(Exception exp){
+            return "Exception in verifying Kibana Search Results:"+exp;
+        }
     }
 
-}
+    public String saveReport(){
+        try{
+            if(seleniumLib.isElementPresent(saveButton)){
+                seleniumLib.clickOnWebElement(saveButton);
+                seleniumLib.sleepInSeconds(2);
+                seleniumLib.sendValue(reportNameInput,"Report_"+getCurrentTime());
+                if(seleniumLib.isElementPresent(confirmSaveButton)){
+                    seleniumLib.clickOnWebElement(confirmSaveButton);
+                    return "Success";
+                }else{
+                    return "Could not find Confirm Save Button";
+                }
+            }else{
+                return "Could not find Save Button";
+            }
+        }catch(Exception exp){
+            return "Exception in saving the report."+exp;
+        }
+    }
+    public String shareCSVReport(){
+        try{
+            if(seleniumLib.isElementPresent(shareButton)){
+                seleniumLib.clickOnWebElement(shareButton);
+                seleniumLib.sleepInSeconds(2);
+                if(seleniumLib.isElementPresent(csvReports)){
+                    seleniumLib.clickOnWebElement(csvReports);
+                    return "Success";
+                }else{
+                    return "Could not find CSV under share report";
+                }
+            }else{
+                return "Could not find Share Button";
+            }
+        }catch(Exception exp){
+            return "Exception in sharing the report."+exp;
+        }
+    }
+    public String generateCSVReport(){
+        try{
+            if(seleniumLib.isElementPresent(generateCSV)){
+                seleniumLib.clickOnWebElement(generateCSV);
+                seleniumLib.sleepInSeconds(4);
+                return "Success";
+            }else{
+                return "Could not find Generate CSV report";
+            }
+        }catch(Exception exp){
+            return "Exception in Generating the CSV report."+exp;
+        }
+    }
+    public static String getCurrentTime() {
+        Calendar today = Calendar.getInstance();
+        String year = "";
+        String month = "";
+        String day = "";
+        int iyear = today.get(Calendar.YEAR);
+        int imonth = today.get(Calendar.MONTH) + 1;
+        int iday = today.get(Calendar.DATE);
+        int ihour = today.get(Calendar.HOUR);
+        int imin = today.get(Calendar.MINUTE);
+        if (imonth < 10) {
+            month = "0" + imonth;
+        } else {
+            month = "" + imonth;
+        }
+        year = "" + iyear;
+        if (iday < 10) {
+            day = "0" + iday;
+        } else {
+            day = "" + iday;
+        }
+        return day+month+year+ihour+ imin;
+    }
+}//end
--- src/test/java/co/uk/gel/proj/steps/AppStepDefs.java
+++ src/test/java/co/uk/gel/proj/steps/AppStepDefs.java
@@ -30,42 +30,4 @@ public class AppStepDefs extends Pages {
     public void theUserLoginToTheApplicationWithValidCredentials() throws Throwable {
         Debugger.println("User login to application with valid credentials.");
     }
-
-    @Given("a web browser is at the kibana app search page")
-    public void aWebBrowserIsAtTheKibanaAppSearchPage(List<String> attributeOfUrl) {
-        boolean testResult=false;
-        String baseURLKibana = attributeOfUrl.get(0);
-        String KibanaPage = AppConfig.getPropertyValueFromPropertyFile(baseURLKibana);
-        Wait.forPageToBeLoaded(driver);
-        driver.get(KibanaPage);
-//        Wait.forPageToBeLoaded(driver);
-        String navigatedURL = driver.getCurrentUrl();
-        Debugger.println("Current URL before LOGIN is :" + navigatedURL);
-        Wait.seconds(20);
-//        testResult = ;
-        Assert.assertTrue(testResult);
-    }
-
-    @Then("the user search for referral id in search box {string}")
-    public void theUserSearchForReferralIdInSearchBox(String refID) {
-        boolean testResult = false;
-        testResult = kibanaPage.searchReferralId(refID);
-        Assert.assertTrue(testResult);
-    }
-
-    @Then("the user clicks on {string} button")
-    public void theUserClicksOnButton(String button) {
-        boolean testResult = false;
-        testResult = kibanaPage.clickOnButton(button);
-        Assert.assertTrue(testResult);
-    }
-
-    @Then("the user clicks on previous time window and selects the date link")
-    public void theUserClicksOnPreviousTimeWindowAndSelectsTheLink(List<String> link) {
-        boolean testResult = false;
-        testResult = kibanaPage.selectTheDateLink(link);
-        Assert.assertTrue(testResult);
-    }
-
-    //appHome.testMethod step removed as it is not part of current applciation
 }//end
