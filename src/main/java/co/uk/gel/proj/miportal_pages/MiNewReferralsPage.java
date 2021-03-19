e1bdc26f3ad10122a853ab31567f9daf63ceb54b
shafeenkm <33949557+shafeenkm@users.noreply.github.com>
2021-03-19 15:41:14 +0530

    Wait updated

--- src/main/java/co/uk/gel/proj/miportal_pages/MiNewReferralsPage.java
+++ src/main/java/co/uk/gel/proj/miportal_pages/MiNewReferralsPage.java
@@ -67,7 +67,7 @@ public class MiNewReferralsPage<checkTheErrorMessagesInDOBFutureDate> {
     }
     public boolean selectNewReferralsDropDownSearchValue(String value) {
         try {
-            Wait.seconds(5);
+            Wait.seconds(2);
             return seleniumLib.selectFromListByText(newReferralsSearchValue,value);
         } catch (Exception exp) {
             Debugger.println("Exception in MIPortalNewReferrals:selectDropDownSearchValue: "+ exp);
