ec980dd44fb3a612ae1410564416982d664a730e
afinnerty <annie.finnerty@genomicsengland.co.uk>
2021-03-23 16:53:41 +0000

    Updated PDF Form versions and downloading PDFS

--- src/test/java/co/uk/gel/proj/runner/RunnerTest.java
+++ src/test/java/co/uk/gel/proj/runner/RunnerTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
          plugin = {"pretty", "html:target/cucumber","json:target/cucumber.json"},
          glue = {"co.uk.gel.proj.steps"},
          features = {"src/test/resources/features"},
-         tags = {"@BVT_UI_SMOKE_TEST_PACK"}
+         tags = {"@NTS-4703"}
         )
     public class RunnerTest {
         @BeforeClass
