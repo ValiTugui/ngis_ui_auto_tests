a43816fbe57330ba6450ef533c8e2154abde9b88
riteshr7 <57976695+riteshr7@users.noreply.github.com>
2021-09-13 15:32:14 +0530

    Commit for new CI updates in Cancer Jsons.

--- src/test/java/co/uk/gel/proj/steps/ReferralFromJsonSteps.java
+++ src/test/java/co/uk/gel/proj/steps/ReferralFromJsonSteps.java
@@ -432,7 +432,9 @@ public class ReferralFromJsonSteps extends Pages {
                 Assert.fail("Could not save Tumours information.");
             }
             String tumourPresentation = String.valueOf(referralObject.getCancerParticipant().getTumours().get(i).getTumourPresentation());
-            tumourPresentation = tumourPresentation.replace("_", " ");
+            if (tumourPresentation.contains("_")) {
+                tumourPresentation = tumourPresentation.replace("_", " ");
+            }
             tumourPresentation = TestUtils.convertUpperCaseJSONDataToProperFormat(tumourPresentation);
             testResult = tumoursPage.selectTumourFirstPresentationOrOccurrenceValue(tumourPresentation);
             if (!testResult) {
@@ -913,7 +915,6 @@ public class ReferralFromJsonSteps extends Pages {
     }
 
     private void fillStageClinicalQuestions(Referral referralObject) {
-//        driver.get("https://test-ordering.e2e-latest.ngis.io/test-order/referral/r21256076464/requesting-organisation?newReferral=true");
         String stageName = "Clinical questions";
         boolean testResult = referralPage.navigateToStage(stageName);
         if (!testResult) {
