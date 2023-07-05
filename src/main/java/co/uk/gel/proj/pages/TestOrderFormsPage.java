5a25cda8ab404c805634e8da28c819bd0b4e27ea
valitugui <vasilica.tugui@genomicsengland.co.uk>
2023-07-05 16:35:06 +0100

    Fixed TestOrderForms upload files

--- src/main/java/co/uk/gel/proj/pages/TestOrderFormsPage.java
+++ src/main/java/co/uk/gel/proj/pages/TestOrderFormsPage.java
@@ -83,12 +83,12 @@ public class TestOrderFormsPage {
                     return false;
                 }
             }else{
-                LocalFileDetector detector = new LocalFileDetector();
-                ((RemoteWebElement)chooseFilesButton).setFileDetector(detector);
-                File file1 = detector.getLocalFile("./testdata/testfile.pdf");
-                File file2 = detector.getLocalFile("./testdata/testfile2.pdf");
-//                String file1 = new File("testfile.pdf").getAbsolutePath();
-//                String file2 = new File("testfile2.pdf").getAbsolutePath();
+//                LocalFileDetector detector = new LocalFileDetector();
+//                ((RemoteWebElement)chooseFilesButton).setFileDetector(detector);
+//                File file1 = detector.getLocalFile("./testdata/testfile.pdf");
+//                File file2 = detector.getLocalFile("./testdata/testfile2.pdf");
+                String file1 = new File("./testdata/testfile.pdf").getAbsolutePath();
+                String file2 = new File("./testdata/testfile2.pdf").getAbsolutePath();
 //                String file2 = uploadFilepath + "testfile2.pdf" + "\n";
 //                String file3 = uploadFilepath + "testfile_11MB.jpg" + "\n";
 //                String file4 = uploadFilepath + "assentform.pdf" + "\n";
@@ -98,7 +98,7 @@ public class TestOrderFormsPage {
 //                    filePath += uploadFilepath + fileName + "\n";
 //                }
 //                filePath = uploadFilepath + "testfile.pdf";
-                chooseFilesButton.sendKeys(file1.getAbsolutePath() , "\n", file2.getAbsolutePath());
+                chooseFilesButton.sendKeys(file1 , "\n", file2);
 //                if (!seleniumLib.upload(chooseFilesButton, filePath)) {
 //                    Debugger.println("Could not upload the file from BS:" + filePath);
 //                    return false;
